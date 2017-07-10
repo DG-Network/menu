bc9d6cdf207a05b0c7f5ee46c35cab36b3eedd2a
DG-Network <DG-Network@users.noreply.github.com>
2017-07-10 22:12:19 +0800

    Update script-menu.sh

--- script-menu.sh
+++ script-menu.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
-clear
-
-# get the VPS IP
-#ip=`ifconfig venet0:0 | grep 'inet addr' | awk {'print $2'} | sed s/.*://`
-MYIP=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
-#MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
-if [ "$MYIP" = "" ]; then
-	MYIP=$(wget -qO- ipv4.icanhazip.com)
+if [[ $USER != 'root' ]]; then
+	echo "Maaf, Anda harus menjalankan ini sebagai root"
+	exit
 fi
 
-#myip=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0' | head -n1`;
+MYIP=$(wget -qO- ipv4.icanhazip.com)
 
-flag=0
+#vps="soned";
+vps="soned";
 
-echo
+if [[ $vps = "soned" ]]; then
+	source="https://raw.githubusercontent.com/DG-Network"
+else
+	source="https://raw.githubusercontent.com/DG-Network"
+fi
 
+# go to root
+cd
+
+# check registered ip
+wget -q -O IP $source/register/master/IP.txt
+if ! grep -w -q $MYIP IP; then
+	echo "Maaf, hanya IP yang terdaftar yang bisa menggunakan script ini!!"
+	echo "Hubungi: Rizal Hidayat (fb.com/admin.soned atau 081515292117)"
+	rm -f /root/IP
+	exit
+fi
+
+while :
+do
 	#MYIP=$(ifconfig | grep 'inet addr:' | grep -v inet6 | grep -vE '127\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d: -f2 | awk '{ print $1}' | head -1)
 	#if [ "$MYIP" = "" ]; then
 		#MYIP=$(wget -qO- ipv4.icanhazip.com)
